69df76e402a2c5123209fe8f5fc330025daabb45
Zanqeon <tim_huijg@hotmail.com>
2017-04-25 18:09:36 +0200

    Add standard deviation + deviation difference of taking first 4/8/12 workers

--- preprocessing.R
+++ preprocessing.R
@@ -13,12 +13,59 @@ for (idx in 1:max_workers) {
 }
 dataset$max_workers <- 0
 dataset$std_dev <- 0
+dataset$std_dev_4 <- 0
+dataset$std_dev_8 <- 0
+dataset$std_dev_12 <- 0
+
+dif_std_dev_4 <- 0
+dif_std_dev_8 <- 0
+dif_std_dev_12 <- 0
+
+dif_std_dev <-0
+
+
 
 # populate data frame
 for (idx in 1:length(dataset[, 1])) {
+  vector <- c()
   split <- strsplit(dataset$rating_list[idx], ",")[[1]]
   dataset$max_workers[idx] <- length(split)
+  
   for (jdx in 1:length(split)) {
     dataset[idx, paste("worker", jdx, sep = "_")] <- as.numeric(split[jdx])
+    vector <- append(vector, as.numeric(split[jdx]))
+
+  dataset$std_dev[idx] <- sd (vector)
   }
+  
+  vector <- c()
+  for (jdx in 1:4) {
+    vector <- append(vector, as.numeric(split[jdx]))
+    
+  dataset$std_dev_4[idx] <- sd (vector)
+
+  }
+  
+  vector <- c()
+  for (jdx in 1:8) {
+    vector <- append(vector, as.numeric(split[jdx]))
+    
+  dataset$std_dev_8[idx] <- sd (vector)
+  }
+  
+  vector <- c()
+  for (jdx in 1:12) {
+    vector <- append(vector, as.numeric(split[jdx]))
+    
+  dataset$std_dev_12[idx] <- sd (vector)
+  }
+  
+  dif_std_dev_4 <- dif_std_dev_4 + abs(dataset$std_dev_4[idx] - dataset$std_dev[idx])
+  dif_std_dev_8 <- dif_std_dev_8 + abs(dataset$std_dev_8[idx] - dataset$std_dev[idx])
+  dif_std_dev_12 <- dif_std_dev_12 + abs(dataset$std_dev_12[idx] - dataset$std_dev[idx])
+  
+
 }
+dif_std_dev_4 <- dif_std_dev_4 / length(dataset[, 1])
+dif_std_dev_8 <- dif_std_dev_8 / length(dataset[, 1])
+dif_std_dev_12 <- dif_std_dev_12 / length(dataset[, 1])
